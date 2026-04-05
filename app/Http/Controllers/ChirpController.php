51b9ea29a851e5fc1b736c0281cbe960e0294211
shayan khoshrou <shayankhoshrou@shayans-MacBook-Air.local>
2026-04-05 10:40:20 +0200

    the form has added

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -17,4 +17,22 @@ class ChirpController extends Controller
 
         return view('home', ['chirps' => $chirps]);
     } 
-} // <--- 4. Class ends here
+ // <--- 4. Class ends here
+
+public function store(Request $request)
+{
+    $validated = $request->validate([
+        'message' => 'required|string|max:255',
+    ], [
+        'message.required' => 'Please write something to chirp!',
+        'message.max' => 'Chirps must be 255 characters or less.',
+    ]);
+
+    \App\Models\Chirp::create([
+        'message' => $validated['message'],
+        'user_id' => null,
+    ]);
+
+    return redirect('/')->with('success', 'Your chirp has been posted!');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index']);
+Route::post('/chirps', [ChirpController::class, 'store']);
+
